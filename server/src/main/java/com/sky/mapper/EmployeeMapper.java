9b5509c29022ac9673abd0811fbd9cf4d8eb773b
谪仙 <9114195+zhexianplus@user.noreply.gitee.com>
2024-10-08 11:12:33 +0800

    新增员工

--- server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,7 +1,9 @@
 package com.sky.mapper;
 
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
@@ -15,4 +17,10 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+
+    //TODO 插入失败 id也会自增
+    @Insert("insert into employee (name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) "+
+            "values "+
+            " (#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    void insert(Employee employee);
 }
