d194fb636b95e19d5f69e55025b02839e8a6ccb6
谪仙 <9114195+zhexianplus@user.noreply.gitee.com>
2024-10-09 15:43:36 +0800

    AOP、反射公共字段

--- server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,9 +1,11 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -28,6 +30,7 @@ public interface EmployeeMapper {
     @Insert("insert into employee (name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) "+
             "values "+
             " (#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    @AutoFill(OperationType.INSERT)
     void insert(Employee employee);
 
     /**
@@ -41,6 +44,7 @@ public interface EmployeeMapper {
      * 员工账号禁用
      * @param employee
      */
+    @AutoFill(OperationType.UPDATE)
     void updateStatus(Employee employee);
 
     /**
